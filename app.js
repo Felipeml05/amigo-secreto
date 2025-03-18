e512efea34dcd8f9ffa8fccb999e9d364325f9b3
Felipe Morales <fmoraleslizarazo@gmail.com>
2025-03-18 13:59:44 -0500

    Funciones para agregar nombre, validar entrada, visualisar la lista y sortear el amigo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    if (nombre == ""){
+        alert("Por favor ingrese un nombre valido");
+    }else{
+        listaNombres.push(nombre);
+        let lista = document.getElementById('listaAmigos');
+        lista.innerHTML += "<li>" + nombre + "</li>";
+        limpiarCaja();
+    }
+};
+ 
+function sortearAmigo(){
+    let azar = Math.floor(Math.random()*listaNombres.length);
+    let amigoElegido = listaNombres[azar];
+    document.getElementById('listaAmigos').style.display = "none";
+    let listaResultado = document.getElementById('resultado');
+    listaResultado.innerHTML += "<li>" + amigoElegido + "</li>";
+    document.getElementById('sortear').setAttribute('disabled','true');
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
+
+
